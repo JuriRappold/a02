07f1ba9e5a7a860648c2b038ef603791a0e5516b
Minmi345 <aladyina.varvara@gmail.com>
2025-10-12 19:13:54 +0200

    added constructor for highscore

---
+++ program/high_score.py
@@ -0,0 +1,10 @@
+class HighScore:
+    '''Dictionary containing  playerID:Player'''
+    chart = {1:23, 2:15}
+
+    def __init__(self, chart):
+        self.chart = chart
+
+    def get_chart():
+        
+        return chart
