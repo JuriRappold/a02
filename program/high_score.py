1e855eaab63f405fa49116d2b0dcf25f5eace409
Minmi345 <aladyina.varvara@gmail.com>
2025-10-12 21:15:46 +0200

    prints sorted table, return string

--- program/high_score.py
+++ program/high_score.py
@@ -9,14 +9,30 @@ class HighScore:
 
     def get_chart(self):
         # sort dictionary in descending order
-        chart = dict(sorted(self.chart.items(), key=lambda item: item[1], reverse=True))
+        scores = dict(sorted(self.chart.items(), key=lambda item: item[1], reverse=True))
         #dict -> convert tuples into dictionary back
         # sorted() -> sort those tuples using a specific rule  
         #self.chart.items() -> get dictionary as tuples
         #key=lambda item -> iterate through each item
         #item[1] -> iterate through item value
         #reverse=True -> do it in descending, not ascending order
-        return chart
+        
+        table = '''
+        ╔════════════════════════════╗
+        ║       🏅 LEADERBOARD       ║
+        ╠════════════════════════════╣
+        '''
+
+        lines = map(lambda score: f"║ {score[0]:>2}. {score[1][0]:<14} │ {score[1][1]:>5} ║\n        ", enumerate(scores.items(), 1))
+        
+        for line in lines:
+            table += line
+
+        table += '╚════════════════════════════╝'
+
+
+        print(table)
+        return table
     
 if __name__ == "__main__":
     scores = HighScore({1:28, 2:19})
