5bdd440dd6331d484e1d205e1eaa8017b52dbf81
JuriRappold <juri@rappold.berlin>
2025-10-17 12:48:56 +0200

    ran the black code formatting tool on the python files

--- program/dice.py
+++ program/dice.py
@@ -6,14 +6,14 @@ import random
 
 
 class Dice:
-
-    '''
+    """
     static Dice class, only really need the roll dice method;
     could be a static class? --> "easiest" way would just be a python file
     with functions
     done it the "Java" way, source:
     https://stackoverflow.com/questions/30556857/creating-a-static-class-with-no-instances
-    '''
+    """
+
     # Attributes
     # static class wide variable, "real" dice faces will be added later
     # DO NOT CHANGE LENGTH!! Only change to get new dice faces
@@ -37,6 +37,6 @@ class Dice:
         """
         if isinstance(roll, int):
             if 0 < roll < 7:
-                return Dice.__STRING_DICE_FACE[roll-1]
+                return Dice.__STRING_DICE_FACE[roll - 1]
             raise IndexError("Roll out of Bounds")
         raise TypeError("Roll is not an integer!")
