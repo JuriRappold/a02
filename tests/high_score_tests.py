d75a3cd1e1c39ab9fc6496d128b90398fc141601
Gunnar Juan Manuel Arias <gunnar_juan_manuel.arias0009@stud.hkr.se>
2025-10-18 14:28:39 +0200

    Implement (more like update lmao) tests for each function of HighScore class

--- tests/high_score_tests.py
+++ tests/high_score_tests.py
@@ -8,9 +8,10 @@ def test_get_chart_returns_string():
     res = scores.get_chart()
     assert isinstance(res, str)
 
+
 def test_get_chart_input_non_dictionary():
     """Test that get_chart detects non-dictionary value"""
-    scores = HighScore('heheeee >:]')
+    scores = HighScore("heheeee >:]")
     res = scores.get_chart()
     exp = "Wrong value/empty dictionary, unable generate table"
     assert exp == res
@@ -28,7 +29,7 @@ def test_get_chart_input_small_dictionary():
     """Test that get_chart contains proper values"""
     test_dict = {"Rob": 28, "Moth": 19}
     scores = HighScore(test_dict).get_chart()
-    #CHECKS ONLY FOR NAMES, NOT VALUES
+    # CHECKS ONLY FOR NAMES, NOT VALUES
     exp = list(test_dict)
     print(exp)
     for word in exp:
@@ -37,7 +38,7 @@ def test_get_chart_input_small_dictionary():
 
 def test_get_chart_input_big_dictionary():
     """Test that get_chart returns multiple players"""
-    test_dict = {"Rob": 28, "Moth": 19, "RuX":56, "Yuwch":98}
+    test_dict = {"Rob": 28, "Moth": 19, "RuX": 56, "Yuwch": 98}
     scores = HighScore(test_dict).get_chart()
     print(scores)
     exp = list(test_dict)
@@ -47,10 +48,11 @@ def test_get_chart_input_big_dictionary():
 
 def test_sort_dict_input_unsorted():
     """Test that sort_chart sorts properly"""
-    test_dict ={1:15, 4:19, 2:18}
+    test_dict = {1: 15, 4: 19, 2: 18}
     res = HighScore.sort_dict(test_dict)
     exp = {1: 15, 2: 18, 4: 19}
     assert res == exp
+
 
 def test_sort_dict_input_int():
     """Test that sort_chart can handle int"""
@@ -68,12 +70,13 @@ def test_sort_dict_input_None():
 
 def test_sort_dict_input_list():
     """Test that sort_chart can handle list"""
-    res = HighScore.sort_dict([15,14,124,3456,567,'apple','ado'])
+    res = HighScore.sort_dict([15, 14, 124, 3456, 567, "apple", "ado"])
     exp = {}
     assert res == exp
 
+
 def test_sort_dict_input_tuple():
     """Test that sort_chart can handle tuple"""
-    res = HighScore.sort_dict((1,4,3,'f'))
+    res = HighScore.sort_dict((1, 4, 3, "f"))
     exp = {}
     assert res == exp
