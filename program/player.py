b047b82959a6f00b45e231c1ab34e6a473508130
Minmi345 <aladyina.varvara@gmail.com>
2025-10-20 18:23:22 +0200

    added functionality to roll_dice()

--- program/player.py
+++ program/player.py
@@ -1,25 +1,28 @@
-import dice
+from dice import Dice
+#from dice_hand import DiceHand
 
 class Player:
     _next_id = 0
-    #__username = ''
-    #__player_id = 0
-    #__total_score = 0
-    # _diceHand = DiceHand()
 
     def __init__(self,name):
-        self.__username = name
-        self.__player_id = Player._next_id
-        print(Player._next_id)
-        Player._next_id += 1
+        if isinstance(name,str) and name != '':
+            self.__username = name
+            self.__player_id = Player._next_id
+            # self._diceHand = DiceHand()
+            self.__total_score = 0
+            print(Player._next_id)
+            Player._next_id += 1
+        else: raise TypeError("name should be string!")
 
-    def roll_dice():
-        #why this one is even on UML? it's dice, not player thing to do...
-        #I understand save in dicehand, but not this
-        pass
+    def roll_dice(self):
+        dice = Dice.roll_dice()
+        #self._diceHand.append(dice)
+        return dice
 
     def change_username(self,new_name):
-        self.__username = new_name
+        if isinstance(new_name,str) and new_name != '':
+            self.__username = new_name
+        else: raise TypeError("name should be string!")
 
     def get_username(self):
         return self._username
